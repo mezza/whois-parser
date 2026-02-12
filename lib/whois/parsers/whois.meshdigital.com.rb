f2b28102e173e32646ee10559ca8add07e6b2d0b
Merul Patel <merul@me.com>
2026-02-12 17:38:57 +0000

    Adding parser for whois.meshdigital.com
    Although they do provide a RDAP response as well

---
+++ lib/whois/parsers/whois.meshdigital.com.rb
@@ -0,0 +1,47 @@
+#--
+# Ruby Whois
+#
+# An intelligent pure Ruby WHOIS client and parser.
+#
+# Copyright (c) 2009-2022 Simone Carletti <weppos@weppos.net>
+#++
+
+
+require_relative 'base_icann_compliant'
+
+
+module Whois
+  class Parsers
+
+    # Parser for the whois.meshdigital.com server.
+    #
+    # @see Whois::Parsers::Example
+    #   The Example parser for the list of all available methods.
+    #
+    class WhoisMeshdigitalCom < BaseIcannCompliant
+
+      protected
+
+      def build_contact(element, type)
+        if node("#{element} Name") || node("#{element} Country") || node("#{element} Email")
+          Parser::Contact.new(
+            type:         type,
+            id:           node("Registry #{element} ID").presence,
+            name:         value_for_property(element, 'Name'),
+            organization: contact_organization_attribute(element),
+            address:      contact_address_attribute(element),
+            city:         value_for_property(element, 'City'),
+            zip:          value_for_property(element, 'Postal Code'),
+            state:        value_for_property(element, 'State/Province'),
+            country_code: value_for_property(element, 'Country'),
+            phone:        value_for_phone_property(element, 'Phone'),
+            fax:          value_for_phone_property(element, 'Fax'),
+            email:        value_for_property(element, 'Email')
+          )
+        end
+      end
+
+    end
+
+  end
+end
