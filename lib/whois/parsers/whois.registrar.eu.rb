fb93819a68fbe68140cc2b2a8542fb70587f1f4b
Merul Patel <merul@me.com>
2026-02-12 10:12:03 +0000

    Adding parser for whois.registrar.eu

---
+++ lib/whois/parsers/whois.registrar.eu.rb
@@ -0,0 +1,25 @@
+#--
+# Ruby Whois
+#
+# An intelligent pure Ruby WHOIS client and parser.
+#
+# Copyright (c) 2009-2022 Simone Carletti <weppos@weppos.net>
+#++
+
+
+require_relative 'base_icann_compliant'
+
+
+module Whois
+  class Parsers
+
+    # Parser for the whois.registrar.eu server.
+    #
+    # @see Whois::Parsers::Example
+    #   The Example parser for the list of all available methods.
+    #
+    class WhoisRegistrarEu < BaseIcannCompliant
+    end
+
+  end
+end
